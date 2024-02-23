51bfe34d893fe5b941263f9a991fc9516baf1fc9
Victor Azevedo <victor.azevedo@mail.com>
2024-02-23 11:00:17 +0100

    Var to let, replaced comments with JSDoc types

--- src/index.js
+++ src/index.js
@@ -4,98 +4,139 @@ if (!this.sh_languages) {
 	this.sh_languages = {}
 }
 
+/**
+ * A language definition object
+ * @typedef {[RegExp, string, number, number]} Language
+ */
+
+/**
+ * A tag object with its position
+ * @typedef {{node: Node, pos: number}} Tag
+ */
+
+/**
+ * @param {string} url
+ * @returns {boolean}
+ */
 function sh_isEmailAddress(url) {
 	if (/^mailto:/.test(url)) {
 		return false
 	}
+
 	return url.indexOf('@') !== -1
 }
 
+/**
+ * @param {Tag[]} tags
+ * @param {number} numTags
+ * @param {string} inputString
+ */
 function sh_setHref(tags, numTags, inputString) {
-	var url = inputString.substring(tags[numTags - 2].pos, tags[numTags - 1].pos)
+	const startPos = tags[numTags - 2].pos
+	const endPos = tags[numTags - 1].pos
+	let url = inputString.substring(startPos, endPos)
+
 	if (url.length >= 2 && url.charAt(0) === '<' && url.charAt(url.length - 1) === '>') {
-		url = url.substr(1, url.length - 2)
+		url = url.substring(1, url.length - 2)
 	}
+
 	if (sh_isEmailAddress(url)) {
 		url = 'mailto:' + url
 	}
+
 	tags[numTags - 2].node.href = url
 }
 
 /**
-Highlights all elements containing source code in a text string.  The return
-value is an array of objects, each representing an HTML start or end tag.  Each
-object has a property named pos, which is an integer representing the text
-offset of the tag. Every start tag also has a property named node, which is the
-DOM element started by the tag. End tags do not have this property.
-@param  inputString  a text string
-@param  language  a language definition object
-@return  an array of tag objects
-*/
+ * Highlights all elements containing source code in a text string.  The return
+ * value is an array of objects, each representing an HTML start or end tag.  Each
+ * object has a property named pos, which is an integer representing the text
+ * offset of the tag. Every start tag also has a property named node, which is the
+ * DOM element started by the tag. End tags do not have this property.
+ * @param {string} inputString - a text string
+ * @param {Language} language - a language definition object
+ * @return an array of tag objects
+ */
 function sh_highlightString(inputString, language) {
-	var a = document.createElement('a')
-	var span = document.createElement('span')
+	const a = document.createElement('a')
+	const span = document.createElement('span')
 
-	// the result
-	var tags = []
-	var numTags = 0
+	/**
+	 * Result tags
+	 * @type {{node: Node, pos: number}[]} */
+	let tags = []
 
-	// each element is a pattern object from language
-	var patternStack = []
+	let numTags = 0
+
+	/**
+	 * Each element is a pattern object from language
+	 * @type {[RegExp, string, number, number][]} */
+	let patternStack = []
 
 	// the current position within inputString
-	var pos = 0
+	let pos = 0
 
 	// the name of the current style, or null if there is no current style
-	var currentStyle = null
+	let currentStyle = null
 
-	var output = function (s, style) {
-		var length = s.length
+	const output = (s, style) => {
+		const length = s.length
+
 		// this is more than just an optimization - we don't want to output empty <span></span> elements
 		if (length === 0) {
 			return
 		}
+
 		if (!style) {
-			var stackLength = patternStack.length
+			const stackLength = patternStack.length
+
 			if (stackLength !== 0) {
-				var pattern = patternStack[stackLength - 1]
-				// check whether this is a state or an environment
-				if (!pattern[3]) {
-					// it's not a state - it's an environment; use the style for this environment
+				const pattern = patternStack[stackLength - 1]
+				const isEnvironnement = !pattern[3]
+
+				// use the style for this environment
+				if (isEnvironnement) {
 					style = pattern[1]
 				}
 			}
 		}
+
 		if (currentStyle !== style) {
 			if (currentStyle) {
-				tags[numTags++] = { pos: pos }
+				tags[numTags++] = { pos }
+
 				if (currentStyle === 'sh_url') {
 					sh_setHref(tags, numTags, inputString)
 				}
 			}
+
 			if (style) {
-				var clone
-				if (style === 'sh_url') {
-					clone = a.cloneNode(false)
-				} else {
-					clone = span.cloneNode(false)
-				}
+				const isUrl = style === 'sh_url'
+				const clone = (isUrl ? a : span).cloneNode(false)
+
 				clone.className = style
-				tags[numTags++] = { node: clone, pos: pos }
+
+				tags[numTags++] = {
+					node: clone,
+					pos: pos,
+				}
 			}
 		}
+
 		pos += length
 		currentStyle = style
 	}
 
-	var endOfLinePattern = /\r\n|\r|\n/g
+	const endOfLinePattern = /\r\n|\r|\n/g
 	endOfLinePattern.lastIndex = 0
-	var inputStringLength = inputString.length
+	const inputStringLength = inputString.length
+
 	while (pos < inputStringLength) {
-		var start = pos
-		var end
-		var startOfNextLine
-		var endOfLineMatch = endOfLinePattern.exec(inputString)
+		const endOfLineMatch = endOfLinePattern.exec(inputString)
+		let start = pos
+		let end = -1
+		let startOfNextLine
+
 		if (endOfLineMatch === null) {
 			end = inputStringLength
 			startOfNextLine = inputStringLength
@@ -104,42 +145,41 @@ function sh_highlightString(inputString, language) {
 			startOfNextLine = endOfLinePattern.lastIndex
 		}
 
-		var line = inputString.substring(start, end)
+		const line = inputString.substring(start, end)
 
-		var matchCache = []
+		let matchCache = []
+
 		for (;;) {
-			var posWithinLine = pos - start
+			const posWithinLine = pos - start
+			const stackLength = patternStack.length
+			const stateIndex = stackLength > 0 ? patternStack[stackLength - 1][2] : 0
+			const state = language[stateIndex]
+			const numPatterns = state.length
+			let mc = matchCache[stateIndex]
 
-			var stateIndex
-			var stackLength = patternStack.length
-			if (stackLength === 0) {
-				stateIndex = 0
-			} else {
-				// get the next state
-				stateIndex = patternStack[stackLength - 1][2]
-			}
-
-			var state = language[stateIndex]
-			var numPatterns = state.length
-			var mc = matchCache[stateIndex]
 			if (!mc) {
 				mc = matchCache[stateIndex] = []
 			}
-			var bestMatch = null
-			var bestPatternIndex = -1
-			for (var i = 0; i < numPatterns; i++) {
-				var match
+
+			let bestMatch = null
+			let bestPatternIndex = -1
+
+			for (let i = 0; i < numPatterns; i++) {
+				let match
+
 				if (i < mc.length && (mc[i] === null || posWithinLine <= mc[i].index)) {
 					match = mc[i]
 				} else {
-					var regex = state[i][0]
+					let regex = state[i][0]
 					regex.lastIndex = posWithinLine
 					match = regex.exec(line)
 					mc[i] = match
 				}
+
 				if (match !== null && (bestMatch === null || match.index < bestMatch.index)) {
 					bestMatch = match
 					bestPatternIndex = i
+
 					if (match.index === posWithinLine) {
 						break
 					}
@@ -155,12 +195,12 @@ function sh_highlightString(inputString, language) {
 					output(line.substring(posWithinLine, bestMatch.index), null)
 				}
 
-				var pattern = state[bestPatternIndex]
+				let pattern = state[bestPatternIndex]
+				let newStyle = pattern[1]
+				let matchedString
 
-				var newStyle = pattern[1]
-				var matchedString
 				if (newStyle instanceof Array) {
-					for (var subexpression = 0; subexpression < newStyle.length; subexpression++) {
+					for (let subexpression = 0; subexpression < newStyle.length; subexpression++) {
 						matchedString = bestMatch[subexpression + 1]
 						output(matchedString, newStyle[subexpression])
 					}
@@ -191,12 +231,15 @@ function sh_highlightString(inputString, language) {
 
 		// end of the line
 		if (currentStyle) {
-			tags[numTags++] = { pos: pos }
+			tags[numTags++] = { pos }
+
 			if (currentStyle === 'sh_url') {
 				sh_setHref(tags, numTags, inputString)
 			}
+
 			currentStyle = null
 		}
+
 		pos = startOfNextLine
 	}
 
@@ -207,16 +250,18 @@ function sh_highlightString(inputString, language) {
 // DOM-dependent functions
 
 /**
-Extracts the tags from an HTML DOM NodeList.
-@param  nodeList  a DOM NodeList
-@param  result  an object with text, tags and pos properties
-*/
+ * Extracts the tags from an HTML DOM NodeList.
+ * @param {NodeListOf<Node>} nodeList - a DOM NodeList
+ * @param {{text: string, pos: number, tags: Tag}} result - an object with text, tags and pos properties
+ */
 function sh_extractTagsFromNodeList(nodeList, result) {
-	var length = nodeList.length
-	for (var i = 0; i < length; i++) {
-		var node = nodeList.item(i)
+	const length = nodeList.length
+
+	for (let i = 0; i < length; i++) {
+		let node = nodeList.item(i)
+
 		switch (node.nodeType) {
-			case 1:
+			case 1: {
 				if (node.nodeName.toLowerCase() === 'br') {
 					result.text.push('\n')
 					result.pos++
@@ -225,64 +270,67 @@ function sh_extractTagsFromNodeList(nodeList, result) {
 					sh_extractTagsFromNodeList(node.childNodes, result)
 					result.tags.push({ pos: result.pos })
 				}
+
 				break
+			}
+
 			case 3:
-			case 4:
+			case 4: {
 				result.text.push(node.data)
 				result.pos += node.length
 				break
+			}
 		}
 	}
 }
 
 /**
-Extracts the tags from the text of an HTML element. The extracted tags will be
-returned as an array of tag objects. See sh_highlightString for the format of
-the tag objects.
-@param  element  a DOM element
-@param  tags  an empty array; the extracted tag objects will be returned in it
-@return  the text of the element
-@see  sh_highlightString
-*/
+ * Extracts the tags from the text of an HTML element. The extracted tags will be
+ * returned as an array of tag objects. See sh_highlightString for the format of
+ * the tag objects.
+ * @param {Element} element - a DOM element
+ * @param {Tag[]} tags - an empty array; the extracted tag objects will be returned in it
+ * @return {string} the text of the element
+ * @see  sh_highlightString
+ */
 function sh_extractTags(element, tags) {
-	var result = {}
-	result.text = []
-	result.tags = tags
-	result.pos = 0
+	const result = { text: [], pos: 0, tags }
 	sh_extractTagsFromNodeList(element.childNodes, result)
 	return result.text.join('')
 }
 
 /**
-Merges the original tags from an element with the tags produced by highlighting.
-@param  originalTags  an array containing the original tags
-@param  highlightTags  an array containing the highlighting tags - these must not overlap
-@result  an array containing the merged tags
-*/
+ * Merges the original tags from an element with the tags produced by highlighting.
+ * @param {Tag[]} originalTags - an array containing the original tags
+ * @param {Tag[]} highlightTags - an array containing the highlighting tags these must not overlap
+ * @result {Tag[]} - an array containing the merged tags
+ */
 function sh_mergeTags(originalTags, highlightTags) {
-	var numOriginalTags = originalTags.length
+	//
+	const numOriginalTags = originalTags.length
 	if (numOriginalTags === 0) {
 		return highlightTags
 	}
 
-	var numHighlightTags = highlightTags.length
+	const numHighlightTags = highlightTags.length
 	if (numHighlightTags === 0) {
 		return originalTags
 	}
 
-	var result = []
-	var originalIndex = 0
-	var highlightIndex = 0
+	let result = []
+	let originalIndex = 0
+	let highlightIndex = 0
 
 	while (originalIndex < numOriginalTags && highlightIndex < numHighlightTags) {
-		var originalTag = originalTags[originalIndex]
-		var highlightTag = highlightTags[highlightIndex]
+		let originalTag = originalTags[originalIndex]
+		let highlightTag = highlightTags[highlightIndex]
 
 		if (originalTag.pos <= highlightTag.pos) {
 			result.push(originalTag)
 			originalIndex++
 		} else {
 			result.push(highlightTag)
+
 			if (highlightTags[highlightIndex + 1].pos <= originalTag.pos) {
 				highlightIndex++
 				result.push(highlightTags[highlightIndex])
@@ -314,25 +362,24 @@ function sh_mergeTags(originalTags, highlightTags) {
 }
 
 /**
-Inserts tags into text.
-@param  tags  an array of tag objects
-@param  text  a string representing the text
-@return  a DOM DocumentFragment representing the resulting HTML
-*/
+ * Inserts tags into text.
+ * @param {Tag[]} tags - an array of tag objects
+ * @param {string} text - a string representing the text
+ * @return {DocumentFragment} a DOM DocumentFragment representing the resulting HTML
+ */
 function sh_insertTags(tags, text) {
-	var doc = document
-
-	var result = document.createDocumentFragment()
-	var tagIndex = 0
-	var numTags = tags.length
-	var textPos = 0
-	var textLength = text.length
-	var currentNode = result
+	const result = document.createDocumentFragment()
+	const numTags = tags.length
+	const textLength = text.length
+	let currentNode = result
+	let tagIndex = 0
+	let textPos = 0
 
 	// output one tag or text node every iteration
 	while (textPos < textLength || tagIndex < numTags) {
-		var tag
-		var tagPos
+		let tag
+		let tagPos
+
 		if (tagIndex < numTags) {
 			tag = tags[tagIndex]
 			tagPos = tag.pos
@@ -344,7 +391,7 @@ function sh_insertTags(tags, text) {
 			// output the tag
 			if (tag.node) {
 				// start tag
-				var newNode = tag.node
+				let newNode = tag.node
 				currentNode.appendChild(newNode)
 				currentNode = newNode
 			} else {
@@ -354,7 +401,7 @@ function sh_insertTags(tags, text) {
 			tagIndex++
 		} else {
 			// output text
-			currentNode.appendChild(doc.createTextNode(text.substring(textPos, tagPos)))
+			currentNode.appendChild(document.createTextNode(text.substring(textPos, tagPos)))
 			textPos = tagPos
 		}
 	}
@@ -363,11 +410,11 @@ function sh_insertTags(tags, text) {
 }
 
 /**
-Highlights an element containing source code.  Upon completion of this function,
-the element will have been placed in the "sh_sourceCode" class.
-@param {Element} element - a DOM <pre> element containing the source code to be highlighted
-@param {Object} language - a language definition object
-*/
+ * Highlights an element containing source code.  Upon completion of this function,
+ * the element will have been placed in the "sh_sourceCode" class.
+ * @param {Element} element - a DOM <pre> element containing the source code to be highlighted
+ * @param {Object} language - a language definition object
+ */
 function sh_highlightElement(element, language) {
 	element.classList.add('sh_sourceCode')
 
@@ -385,28 +432,22 @@ function sh_highlightElement(element, language) {
 }
 
 /**
-Highlights all elements containing source code on the current page. Elements
-containing source code must be "pre" elements with a "class" attribute of
-"sh_LANGUAGE", where LANGUAGE is a valid language identifier; e.g., "sh_java"
-identifies the element as containing "java" language source code.
-*/
+ * Highlights all elements containing source code on the current page. Elements
+ * containing source code must be "pre" elements with a "class" attribute of
+ * "sh_LANGUAGE", where LANGUAGE is a valid language identifier; e.g., "sh_java"
+ * identifies the element as containing "java" language source code.
+ */
 function sh_highlightDocument() {
 	for (const element of document.querySelectorAll('pre')) {
-		for (const classItem of element.classList) {
-			if (classItem.toLowerCase() === 'sh_sourcecode') {
-				continue
-			}
+		const classItem = [...element.classList].find((cl) => cl.startsWith('sh_'))
 
-			if (classItem.substring(0, 3) === 'sh_') {
-				var language = classItem.substring(3).toLowerCase()
+		if (classItem) {
+			const language = classItem.substring(3).toLowerCase()
 
-				if (language in sh_languages) {
-					sh_highlightElement(element, sh_languages[language])
-				} else {
-					throw `Found <pre> element with class="${classItem}", but no such language exists`
-				}
-
-				break
+			if (language in sh_languages) {
+				sh_highlightElement(element, sh_languages[language])
+			} else {
+				throw `Found <pre> element with class="${classItem}", but no such language exists`
 			}
 		}
 	}
